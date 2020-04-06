c782707a7bca1186ca971de81b1691cb278ba68a
Patrick Harl <therandomwoodworker@gmail.com>
2020-04-06 13:48:27 -0700

    completed selectors and content updates

--- js/index.js
+++ js/index.js
@@ -37,6 +37,107 @@ const siteContent = {
   },
 };
 
+
+
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+//nav
+
+console.log(Object.keys(siteContent.nav).length);
+
+let navSec = document.querySelectorAll('nav a');
+
+for (let i = 0; i < Object.keys(siteContent.nav).length - 1; i++)
+{
+
+  const contentNav = Object.values(siteContent.nav);
+  navSec[i].textContent = contentNav[i];
+
+}
+
+//cta
+let ctaHeader = document.querySelector('.cta h1');
+ctaHeader.textContent = siteContent["cta"]["h1"];
+
+let ctaButton = document.querySelector('.cta button');
+ctaButton.textContent = siteContent["cta"]["button"];
+
+let ctaImg = document.getElementById('cta-img');
+ctaImg.src = siteContent["cta"]["img-src"];
+
+//topContent
+let mcTop = document.querySelectorAll('.main-content .top-content .text-content');
+
+let mcTopFeaturesTitle = mcTop[0].querySelector('h4');
+mcTopFeaturesTitle.textContent = siteContent["main-content"]["features-h4"];
+
+let mcTopFeaturesText = mcTop[0].querySelector('p');
+mcTopFeaturesText.textContent = siteContent["main-content"]["features-content"];
+
+let mcTopAboutTitle = mcTop[1].querySelector('h4');
+mcTopAboutTitle.textContent = siteContent["main-content"]["about-h4"];
+
+let mcTopAboutText = mcTop[1].querySelector('p');
+mcTopAboutText.textContent = siteContent["main-content"]["about-content"];
+
+//middleImage
+
+let midImg = document.getElementById('middle-img');
+midImg.src = siteContent["main-content"]["middle-img-src"];
+
+//bottomContent
+
+let mcBottom = document.querySelectorAll('.main-content .bottom-content .text-content');
+
+let mcBottomServicesTitle = mcBottom[0].querySelector('h4');
+mcBottomServicesTitle.textContent = siteContent["main-content"]["services-h4"];
+
+let mcBottomServicesText = mcBottom[0].querySelector('p');
+mcBottomServicesText.textContent = siteContent["main-content"]["services-content"];
+
+let mcBottomProductTitle = mcBottom[1].querySelector('h4');
+mcBottomProductTitle.textContent = siteContent["main-content"]["product-h4"]
+
+let mcBottomProductText = mcBottom[1].querySelector('p');
+mcBottomProductText.textContent = siteContent["main-content"]["product-content"];
+
+let mcBottomVisionTitle = mcBottom[2].querySelector('h4');
+mcBottomVisionTitle.textContent = siteContent["main-content"]["vision-h4"];
+
+let mcBottomVisionText = mcBottom[2].querySelector('p');
+mcBottomVisionText.textContent = siteContent["main-content"]["vision-content"];
+
+//contact
+
+let contact = document.querySelector('.contact');
+
+let contactHeader = contact.querySelector('h4');
+contactHeader.textContent = siteContent["contact"]["contact-h4"];
+
+let contactPara = contact.querySelectorAll('p');
+
+let contactPara1 = contactPara[0];
+contactPara1.textContent = siteContent["contact"]["address"];
+
+let contactPara2 = contactPara[1];
+contactPara2.textContent = siteContent["contact"]["phone"];
+
+let contactPara3 = contactPara[2];
+contactPara3.textContent = siteContent["contact"]["email"];
+
+//footer
+
+let footerPara = document.querySelector('footer p');
+footerPara.textContent = siteContent["footer"]["copyright"];
+
+
+
+
+
+
+
+
+
